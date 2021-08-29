9cefbd3c0b03fa56322edf7c525e80e2ee9846da
Lanlyhs <ziqiang.chen@outlook.com>
2021-08-29 14:55:08 +0800

    Fix ENV variable error

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -55,7 +55,7 @@
      }
    ];
    initExtra = ''
-   if [ -e ${home.homeDirectory}/.nix-profile/etc/profile.d/nix.sh ]; then . ${home.homeDirectory}/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+   if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
    '';
   };
 
