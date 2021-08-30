319983b907e08ab34be2773895744e43b1831eb8
Lanlyhs <ziqiang.chen@outlook.com>
2021-08-30 21:18:41 +0800

    Update use HOME variable

--- tools/update.sh
+++ tools/update.sh
@@ -4,5 +4,6 @@ set -e
 
 nix-shell -p git --run "cd $HOME/.nixconfig; git pull --rebase --stat;"
 
-cp -rf $HOME/.nixconfig/home-manager/* .config/nixpkgs/
+rm -rf ${HOME}/.config/nixpkgs/
+cp -rf $HOME/.nixconfig/home-manager/* $HOME/.config/nixpkgs/
 home-manager switch
