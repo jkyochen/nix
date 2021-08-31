8bad2fce3fd7cb5dfb23de5cbf10c133df963bb7
ZiQiang Chen <ziqiang.chen@outlook.com>
2021-08-31 20:10:45 +0800

    Fix no create directory

--- tools/update.sh
+++ tools/update.sh
@@ -4,6 +4,6 @@ set -e
 
 nix-shell -p git --run "cd $HOME/.nixconfig; git pull --rebase --stat;"
 
-rm -rf ${HOME}/.config/nixpkgs/
+rm -rf ${HOME}/.config/nixpkgs/*
 cp -rf $HOME/.nixconfig/home-manager/* $HOME/.config/nixpkgs/
 home-manager switch
