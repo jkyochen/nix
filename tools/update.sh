a5e104a74df5c83f5434163d4162f30eda542527
Lanlyhs <ziqiang.chen@outlook.com>
2021-10-16 18:07:05 +0800

    Fix cp *

--- tools/update.sh
+++ tools/update.sh
@@ -5,5 +5,5 @@ set -e
 nix-shell -p git --run "cd $HOME/.nixconfig; git pull --rebase --stat;"
 
 rm -rf ${HOME}/.config/nixpkgs/*
-cp -rf $HOME/.nixconfig/home-manager/* $HOME/.config/nixpkgs/
+cp -rf $HOME/.nixconfig/home-manager/ $HOME/.config/nixpkgs/
 home-manager switch
